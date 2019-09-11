53f6c7427cffa5e9aedecee7b1b66b3d9cd379df
Jayne Carmichael Norrie <39460834+jaynecn@users.noreply.github.com>
2019-09-11 19:06:57 +0100

    completed formatting

--- src/PhotoDisplay.js
+++ src/PhotoDisplay.js
@@ -8,9 +8,10 @@ function PhotoDisplay(props) {
       <h1>{name.title}</h1>
       <div className="imageDiv">
         <img className='image' src={name.url} alt={name.title}></img>
-        <h5>Date taken: {name.date} Copyright: {name.copyright}</h5> 
+        <h5>Date: {name.date}  Copyright: {name.copyright}</h5> 
       </div>
       <div className="explanation">
+        <p><strong>{name.title}</strong></p>
         <p>{name.explanation}</p>
       </div>
     </div>
