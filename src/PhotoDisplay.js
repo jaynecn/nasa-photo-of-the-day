d8ac97fbde7ece79154a99bb49d6d0e707f67f10
Jayne Carmichael Norrie <39460834+jaynecn@users.noreply.github.com>
2019-09-11 15:45:04 +0100

    got the details displayed.  Now I'm going to work on styling

--- src/PhotoDisplay.js
+++ src/PhotoDisplay.js
@@ -6,7 +6,13 @@ function PhotoDisplay(props) {
   return (
     <div>
       <h1>{name.title}</h1>
-
+      <div className="imageDiv">
+        <img className='image' src={name.url} alt={name.title}></img>
+        <h5>Date taken: {name.date} Copyright: {name.copyright}</h5> 
+      </div>
+      <div className="explanation">
+        <p>{name.explanation}</p>
+      </div>
     </div>
   );
 }
