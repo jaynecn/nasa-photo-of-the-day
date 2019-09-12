c8e1c38fae6ed4d90fe1351d649ae22db2208e2a
Jayne Carmichael Norrie <39460834+jaynecn@users.noreply.github.com>
2019-09-12 14:32:21 +0100

    added styled components and commented out code I added in CSS to mmake sure the styled components were working

--- src/PhotoDisplay.js
+++ src/PhotoDisplay.js
@@ -1,4 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
+
+//Styled Components
+const StyledImageWidth = styled.img`
+  width: 20vw;`
+
+const StyledExplanation = styled.div`
+  text-align: justify;`
+
+const StyledGhostTitle = styled.p`
+  color: purple;
+  opacity: .4;
+
+  &: hover{
+    opacity: 1;
+  }
+`
+
+
+
+
+
 
 function PhotoDisplay(props) {
   const { name } = props;
@@ -7,13 +29,17 @@ function PhotoDisplay(props) {
     <div>
       <h1>{name.title}</h1>
       <div className="imageDiv">
-        <img className='image' src={name.url} alt={name.title}></img>
-        <h5>Date: {name.date}  Copyright: {name.copyright}</h5> 
+        { name.url ? <StyledImageWidth className='image' src={name.url} alt={name.title}></StyledImageWidth>
+                   : <div> Photo loading... </div>
+        }
+        { name.date ? <h5> Date: {name.date}  Copyright: {name.copyright}</h5>
+                    : <h5> Date unknown  Copyright: {name.copyright}</h5>
+        }  
       </div>
-      <div className="explanation">
-        <p><strong>{name.title}</strong></p>
+      <StyledExplanation>
+        <StyledGhostTitle><strong>{name.title}</strong></StyledGhostTitle>
         <p>{name.explanation}</p>
-      </div>
+      </StyledExplanation>
     </div>
   );
 }
